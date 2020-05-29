70e060171d28a14d991a89effdd0a0454be1d9b9
jhl9617@gmail.com <>
2020-05-29 23:58:56 +0900

    기존 UI 다시 생성

---
+++ SimPad/Assets/scripts/start/ButtonToMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ButtonToMenu : MonoBehaviour
+{
+
+    public void OnClickmain()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
